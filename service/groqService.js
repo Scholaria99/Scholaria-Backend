256ff23f3a9c7ad7f818ecd58a99d17c8b2713da
Scholaria99 <scholaria51@gmail.com>
2025-05-26 21:05:38 +0700

    groqService.js

--- service/groqService.js
+++ service/groqService.js
@@ -3,8 +3,6 @@ const axios = require('axios');
 const GROQ_API_URL = 'https://api.groq.com/openai/v1/chat/completions';
 const GROQ_API_KEY = process.env.GROQ_API_KEY;
 
-console.log('Loaded GROQ_API_KEY:', GROQ_API_KEY);
-
 async function generateSoalDariGroq(materi) {
   try {
 const prompt = `
